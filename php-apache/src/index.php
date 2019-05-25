cf1d41ca28ffe48a1ac975a18544089d74782bfd
Emma Sim-Smith <emmasimsmith@gmail.com>
2019-05-25 17:56:08 +1200

    Made a form

--- php-apache/src/index.php
+++ php-apache/src/index.php
@@ -1,1 +1,23 @@
-<?php phpinfo(); ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+
+<form>
+  <input type="text" name="first" placeholder="First name">
+  <br>
+  <input type="text" name="last" placeholder="Last name">
+  <br>
+  <input type="datetime" name="dob" placeholder="Date of Birth">
+  <br>
+  <input type="text" name="unit" placeholder="Unit">
+  <br>
+  <input type="password" name="pwd" placeholder="Password">
+  <br>
+  <button type="submit" name="submit">Enter</button>
+</form>
+
+</body>
+</html>
