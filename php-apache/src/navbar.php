c5bf908f7bfc591cea586b939b79ddc01797f285
Emma Sim-Smith <emmasimsmith@gmail.com>
2019-07-02 12:51:11 +1200

    Created navbar

---
+++ php-apache/src/navbar.php
@@ -0,0 +1,12 @@
+<div class="navbar">
+  <ul>
+    <li><a href="/">Home</a></li>
+    <li><a href="/unit/createunit.php">Create Unit</a></li>
+    <li><a href="/unit/searchunit.php">View Units</a></li>
+    <li><a href="/individual/createindividual.php">Create Individual</a></li>
+    <li><a href="/individual/searchindividual.php">View Individuals</a></li>
+    <li><a href="/class/createclass.php">Create Class</a></li>
+    <li><a href="/boat/createboat.php">Create Boat</a></li>
+
+  </ul>
+</div>
