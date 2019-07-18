57be6f5dc5a13b847bae94940efb4200305cf719
Emma Sim-Smith <emmasimsmith@gmail.com>
2019-07-19 11:39:24 +1200

    Edited Create Class File

--- php-apache/src/class/createclass.php
+++ php-apache/src/class/createclass.php
@@ -1,5 +1,24 @@
 <?php
+//include navigation bar and connection php files
 include_once '../navbar.php';
+include_once '../connection.php';
+
+//function to echo errors, links and closing the connection to database
+function closeclass($error)
+{
+    if ($error) {
+        echo $error . "</br>";
+    } ?>
+    <br>
+    <a href="/">Return Home</a>
+    <br>
+    <a href="createclass.php">Submit another response</a>
+    <br>
+    <a href="searchclass.php">View all classes</a>
+    <?php
+}
+
+//function for the class form
 function classform()
 {
     ?>
@@ -14,7 +33,7 @@ function classform()
     <input type="text" name="class_name" placeholder="Class Name">
     <br>
     Minimum Age:
-    <input type="number" name="min_age" step="any" placeholder="Minimum Age">
+    <input type="number" name="min_age" placeholder="Minimum Age" step="any">
     <br>
     Maximum Age:
     <input type="number" name="max_age" step="any" placeholder="Maximum Age">
@@ -22,60 +41,113 @@ function classform()
     <button type="submit" name="submit">Enter</button>
   </form>
   </body>
-    <br>
-    <a href="searchclass.php">View all classs</a>
-    </html>
+ </html>
 <?php
 }
 
+//IF FORM SUBMITTED
 if (isset($_POST["submit"])) {
-    include_once '../connection.php';
-    classform();
 
+    //define POST variables from form
     $class_name = mysqli_real_escape_string($conn, $_POST['class_name']);
     $min_age = mysqli_real_escape_string($conn, $_POST['min_age']);
     $max_age = mysqli_real_escape_string($conn, $_POST['max_age']);
+
+    //array for validation errors
     $errors = array();
 
+    //format numbers to one decimal placeholder
+    $min_age_formated = number_format($min_age, 1, '.', ',');
+    $max_age_formated = number_format($max_age, 1, '.', ',');
+
+    //validation of variables
     if (!$class_name) {
         array_push($errors, "Class name must be entered");
     }
-
-    if (preg_match('/[^A-Za-z]/', $class_name)) {
+    if (preg_match('/[^A-Za-z ]/', $class_name)) {
         array_push($errors, "Please enter a valid class name");
     }
+    if (!$min_age_formated) {
+        array_push($errors, "Minimum age must be entered");
+    }
+    if (!is_numeric($min_age_formated)) {
+        array_push($errors, "Please enter a valid minimum age");
+    }
+    if ($min_age_formated < 0) {
+        array_push($errors, "Please enter a positive minimum age");
+    }
+    if (!$max_age_formated) {
+        array_push($errors, "Maximum age must be entered");
+    }
+    if (!is_numeric($max_age_formated)) {
+        array_push($errors, "Please enter a valid maximum age");
+    }
+    if ($max_age_formated < 0) {
+        array_push($errors, "Please enter a positive maximum age");
+    }
+    if ($min_age_formated >= $max_age_formated) {
+        array_push($errors, "The maximum age must be greater than the minimum age");
+    }
 
+    //if not valid echo error and form then exit
     if (count($errors) != 0) {
+        //call form with existing values?>
+        <html>
+          <head>
+              <title>Create Class</title>
+          </head>
+          <h1>Create New Class</h1>
+          <body>
+            <form action="createclass.php" method ="POST">
+              Class Name:
+              <input type="text" name="class_name" value="<?php echo $_POST['class_name']?>" placeholder="Class Name">
+              <br>
+              Minimum Age:
+              <input type="number" name="min_age" value="<?php echo $_POST['min_age'] ?>" placeholder="Minimum Age" step="any">
+              <br>
+              Maximum Age:
+              <input type="number" name="max_age" value="<?php echo $_POST['max_age'] ?>" step="any" placeholder="Maximum Age">
+              <br>
+              <button type="submit" name="submit">Enter</button>
+            </form>
+          </body>
+        </html>
+        <?php
         foreach ($errors as $error) {
-            echo $error . "</br>";
+            $issue = '';
+            $issue = $issue . $error . "</br>";
         }
-        mysqli_close($conn); ?>
-
-        <br>
-        <a href="createclass.php">Submit another response</a>
-        <br>
-        <a href="searchclass.php">View all classes</a>
-        <?php
+        closeclass($issue);
+        mysqli_close($conn);
         exit;
     }
+    //call class form
+    classform();
 
-    $sql = "INSERT INTO regattascoring.CLASS (class_name, min_age, max_age) VALUES ('$class_name', '$min_age','$max_age');";
+    //insert variables into class table, if false echo error and exit
+    $sql = "INSERT INTO regattascoring.CLASS (class_name, min_age, max_age)
+    VALUES ('$class_name', '$min_age','$max_age');";
     if (!mysqli_query($conn, $sql)) {
-        echo "ERROR: Could not add class" . mysqli_error($conn) . "</br>"; ?>
-        <br>
-        <a href="createclass.php">Submit another response</a>
-        <?php
+        $error = "Could not add class";
+        closeclass($error);
+        mysqli_close($conn);
         exit;
     }
 
-    echo $_POST['class_name'] . " Class Created";
-    mysqli_close($conn); ?>
+    //echo class created
+    echo $_POST['class_name'] . " Class Created"; ?>
 
     <br>
-    <a href= <?php echo "viewclass.php?id=$class_id" ?>>Edit <?php echo $row['class_name'] ?></a>
-    <br>
-    <a href="createclass.php">Submit another response</a>
+    <a href= <?php echo "viewclass.php?id=$class_id" ?>>Edit <?php echo
+    $_POST['class_name'] ?></a>
     <?php
+    closeclass($error);
+    mysqli_close($conn);
 } else {
-        classform();
-    }
+    //call class form
+    classform();
+
+    //call class close
+    closeclass($error);
+    mysqli_close($conn);
+}
