07180b039705cb823c1f866193aeed1777c20794
Emma Sim-Smith <emmasimsmith@gmail.com>
2019-08-10 10:56:15 +1200

    Added access to events in front page

--- php-apache/src/index.php
+++ php-apache/src/index.php
@@ -1,13 +1,29 @@
 <?php
-include_once 'navbar.php'; ?>
-<!DOCTYPE html>
+include_once 'navbar.php';
+include_once 'connection.php';
+
+
+
+?>
 <html>
 <head>
     <title>Octo Waddle</title>
 </head>
 <body>
 
-<p>This is the home page</p>
+<h1>This is the home page</h1>
+<a href="/event/createevent.php">Create an event</a>
+<br>
 
+<?php
+//select all from event table
+$sql = "SELECT * FROM regattascoring.EVENT;";
+$result = mysqli_query($conn, $sql);
+if ($result) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo "<a href=/participant/selectparticipant.php?id=" . $row['event_id'] . ">Select " . $row['location'] . " Event</a>";
+        echo "<br>";
+    }
+} ?>
 </body>
 </html>
