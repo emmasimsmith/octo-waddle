497b5c84a3538be14267d1bb000fe87a5ebe4802
Emma Sim-Smith <emmasimsmith@gmail.com>
2019-09-02 12:22:43 +1200

    Create Activity finished

--- php-apache/src/activity/createactivity.php
+++ php-apache/src/activity/createactivity.php
@@ -1,6 +1,10 @@
 <?php
+//include navigation bar, functions and connection php files
 include_once '../navbar.php';
-function activityform()
+include_once '../connection.php';
+include_once '../functions.php';
+
+function activityform($result)
 {
     ?>
   <html>
@@ -21,6 +25,14 @@ function activityform()
       <option value="time">Time</option>
     </select>
     <br>
+    Classes:
+    <br>
+    <?php
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo "<input type='checkbox' name='class[]' value=" . $row['class_id'];
+        echo ">" . $row['class_name'] . "</input>" . "<br>";
+    } ?>
+    <br>
     <button type="submit" name="submit">Enter</button>
   </form>
   </body>
@@ -28,13 +40,16 @@ function activityform()
   <?php
 }
 if (isset($_POST["submit"])) {
-    include_once '../connection.php';
 
+  //POST variables from form
     $activity_name = mysqli_real_escape_string($conn, $_POST['activity_name']);
     $scoring = mysqli_real_escape_string($conn, $_POST['scoring']);
+    $class = $_POST['class'];
 
+    //array for input sanitsation errors
     $errors = array();
 
+    //input sanitsation
     if (!$activity_name) {
         array_push($errors, "Activity name must be entered");
     }
@@ -43,43 +58,104 @@ if (isset($_POST["submit"])) {
         array_push($errors, "Please enter a valid activity name");
     }
 
+    //select all function from specific table
+    $result = selectall($conn, "class_name", "regattascoring.CLASS", "Class", "activity", "Activities");
+
+    //if errors, echo and exit
     if (count($errors) != 0) {
+        //call form with existing values?>
+      <html>
+        <head>
+            <title>Create Activity</title>
+        </head>
+        <h1>Create New Activity</h1>
+        <body>
+          <form action="createactivity.php" method ="POST">
+            Activity Name:
+            <input type="text" name="activity_name" value="<?php echo $_POST['activity_name'] ?>" placeholder="Activity name">
+            <br>
+            Scoring Type:
+            <select name="scoring" placeholder="Calculation Method">
+              <option value="placing" <?php if ($_POST['scoring'] == "placing") {
+            echo " selected";
+        } ?>>Placing</option>
+              <option value="scoring"<?php if ($_POST['scoring'] == "scoring") {
+            echo " selected";
+        } ?>>Scoring</option>
+              <option value="time"<?php if ($_POST['scoring'] == "time") {
+            echo " selected";
+        } ?>>Time</option>
+            </select>
+            <br>
+            Classes:
+            <br>
+            <?php
+            while ($row = mysqli_fetch_assoc($result)) {
+                echo "<input type='checkbox' name='class[]' value=" . $row['class_id'] . " ";
+                foreach ($class as $match) {
+                    if ($row['class_id'] == $match) {
+                        echo "checked";
+                    }
+                }
+                echo " >" . $row['class_name'] . "</input> <br>";
+            } ?>
+            <br>
+            <button type="submit" name="submit">Enter</button>
+          </form>
+        </body>
+      </html>
+      <?php
+      //echo errors from the input sanitsation
+      $issue = "";
         foreach ($errors as $error) {
-            echo $error . "</br>";
+            $issue = $issue . $error . "</br>";
         }
-        mysqli_close($conn); ?>
-        <br>
-        <a href="/">Return Home</a>
-        <br>
-        <a href="createactivity.php">Submit another response</a>
-        <br>
-        <a href="searchactivity.php">View all activitys</a>
-        <?php
+        close($conn, $issue, "activity", "Activities");
         exit;
     }
+    //find how many groups there are
+    $sql = "SELECT COUNT(DISTINCT activity_group) as total FROM regattascoring.ACTIVITY;";
+    $result = mysqli_query($conn, $sql);
+    $groups = mysqli_fetch_assoc($result);
+    $group = $groups['total'];
 
-    $sql = "INSERT INTO regattascoring.ACTIVITY (activity_name, scoring) VALUES
-    ('$activity_name','$scoring');";
+    //increase group by one
+    $group++;
 
-    if (!mysqli_query($conn, $sql)) {
-        echo "ERROR: Could not add data" . mysqli_error($conn) . "</br>";
+    //foreach loop of selected classes
+    foreach ($class as $class_id) {
+        //Insert variables into activity table, if false echo error and exit
+        $sql = "INSERT INTO regattascoring.ACTIVITY (activity_name, scoring, class_id, activity_group) VALUES
+      ('$activity_name','$scoring', '$class_id', '$group');";
+        if (!mysqli_query($conn, $sql)) {
+            close($conn, "Could not add data", "activity", "Activities");
+            exit;
+        }
     }
-    $activity_id = mysqli_insert_id($conn);
-    echo $_POST['activity_name'] . " Activity Created"; ?>
+
+    //echo activity created
+    echo $_POST['activity_name'] . " Activity Created";
+    $activity_id = mysqli_insert_id($conn); ?>
     <br>
-    <a href = <?php echo "viewactivity.php?id=$activity_id"?>>Edit
-      <?php echo $_POST['activity_name'] . " Activity" ?></a>
-    <php>
-    <?php
-    activityform();
-    mysqli_close($conn); ?>
-    <br>
-    <a href="/">Return Home</a>
-    <br>
-    <a href="createactivity.php">Submit another response</a>
-    <br>
-    <a href="searchactivity.php">View all activitys</a>
-    <?php
+    <a href = <?php echo "viewactivity.php?id=$activity_id>Edit ";
+    echo $_POST['activity_name'] . " Activity</a>";
+
+    //call select all function for form
+    $row = selectall($conn, "class_name", "regattascoring.CLASS", "Class", "activity", "Activities");
+
+    //call activity form
+    activityform($row);
+
+    //call closing function
+    close($conn, $error, "actitivty", "Activities");
 } else {
-        activityform();
-    };
+
+  //call select all function for form
+    $row = selectall($conn, "class_name", "regattascoring.CLASS", "Class", "activity", "Activities");
+
+    //call activity form
+    activityform($row);
+
+    //call close
+    close($conn, $error, "actitivty", "Activities");
+}
