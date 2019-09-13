7da7ea0b6d07de6ec6bdbf3cb754ea182c8d4ff4
Emma Sim-Smith <emmasimsmith@gmail.com>
2019-09-14 10:50:03 +1200

    FIXED search for activity

--- php-apache/src/activity/searchactivity.php
+++ php-apache/src/activity/searchactivity.php
@@ -3,6 +3,7 @@
 include_once '../navbar.php';
 include_once '../connection.php';
 include_once '../functions.php';
+include_once '../morefunctions.php';
 
 //Form function
 function activityform()
@@ -12,6 +13,7 @@ function activityform()
   <body>
     <form action= searchactivity.php method="POST">
       <input type="text" name="activity_name" placeholder="Search by activity name">
+      <input type="text" name="activity_bracket" placeholder="Search by activity bracket">
       <input type="text" name="scoring" placeholder="Search by scoring method">
       <input type="text" name="class_name" placeholder="Search by class">
       <button type="submit" name="search">Enter</button>
@@ -28,30 +30,63 @@ if (isset($_POST['search'])) {
 
     //define POST variables
     $search_activity_name = mysqli_real_escape_string($conn, $_POST['activity_name']);
+    $search_activity_bracket = mysqli_real_escape_string($conn, $_POST['activity_bracket']);
     $search_scoring = mysqli_real_escape_string($conn, $_POST['scoring']);
     $search_class_name = mysqli_real_escape_string($conn, $_POST['class_name']);
 
     //validation if strings are empty
-    if (!$_POST['activity_name'] and !$_POST['scoring'] and !$_POST['class_name']) {
+    if (!$_POST['activity_name'] and !$_POST['activity_bracket'] and !$_POST['scoring'] and !$_POST['class_name']) {
         close($conn, "Please search a value", "activity", "Activities");
         exit;
     }
 
-    //variable array for function
-    $variables = array(
-    'activity_name' => array('Activity Name' => $search_activity_name),
-    'scoring' => array('Scoring Method' => $search_scoring),
-    'class_name' => array('Class Name' => $search_class_name));
+    //call search
+    $search = array();
+    $sql = "SELECT * FROM regattascoring.ACTIVITY WHERE ";
+    if ($search_activity_name) {
+        array_push($search, "activity_name LIKE '%$search_activity_name%'");
+    }
+    if ($search_scoring) {
+        array_push($search, "scoring LIKE '%$search_scoring%'");
+    }
+    if ($search_activity_bracket) {
+        array_push($search, "activity_bracket LIKE '%$search_activity_bracket%'");
+    }
+    if ($search_class_name) {
+        //set array
+        $class = array();
+        //search for class within bracket to find activity ids
+        $other = "SELECT activity_id FROM regattascoring.BRACKET NATURAL JOIN
+      regattascoring.CLASS WHERE class_name LIKE '%$search_class_name%';";
+        $result = mysqli_query($conn, $other);
+        while ($row = mysqli_fetch_assoc($result)) {
+            array_push($class, "activity_id = " . $row['activity_id']);
+        }
+        $id_classes = join(" OR ", $class);
+        //array push
+        array_push($search, $id_classes);
+    }
 
-    //call search function
-    search(
-        $conn,
-        "activity",
-        $variables,
-        "regattascoring.ACTIVITY NATURAL JOIN regattascoring.CLASS",
-        "Activity",
-        "Activities"
-    );
+    $join = join(" AND ", $search);
+    $sql = $sql . $join . ";";
+    echo $sql;
+
+    //check if there are rows that match
+    $search = mysqli_query($conn, $sql);
+    if (mysqli_num_rows($search) == 0) {
+        echo "No matches in table" . "</br>";
+        echo "<br>
+        <a href='/'>Return Home</a>
+        <br>
+        <a href= 'viewactivity.php'>Edit Activities</a>
+        <br>
+          <a href='searchactivity.php'>View all Activities</a>";
+        mysqli_close($conn);
+        exit;
+    }
+
+    //call activity view function
+    activity_view($conn, $search);
 
     //call close function
     close($conn, $error, "activity", "Activities");
@@ -59,17 +94,9 @@ if (isset($_POST['search'])) {
     //call activity form
     activityform();
 
-    //variables array
-    $variables = array('activity_name' => 'Activity Name',
-    'scoring' => 'Scoring Method', 'class_name' => 'Class Name');
+    //echo all data from table
+    $sql = "SELECT * FROM regattascoring.ACTIVITY;";
+    $result = mysqli_query($conn, $sql);
 
-    //echo all data from table and close
-    viewall(
-        $conn,
-        "activity",
-        "regattascoring.ACTIVITY NATURAL JOIN regattascoring.CLASS",
-        $variables,
-        "activity_group",
-        "Activities"
-    );
+    activity_view($conn, $result);
 }
