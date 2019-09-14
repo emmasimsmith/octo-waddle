34a60cc4566fdbcf70eace8d5abecd9235b7c25b
Emma Sim-Smith <emmasimsmith@gmail.com>
2019-09-14 21:07:06 +1200

    Pre made values for activties

--- php-apache/src/activity/createactivity.php
+++ php-apache/src/activity/createactivity.php
@@ -1,196 +1,166 @@
 <?php
-//include navigation bar, functions and connection php files
-include_once '../navbar.php';
+//include connection.php
 include_once '../connection.php';
-include_once '../functions.php';
+include_once '../navbar.php';
 
-//get event id from GET
-$event_id = $_GET['event_id'];
-
-function activityform($result, $event_id)
-{
-    $sql = "SELECT * FROM regattascoring.EVENT WHERE event_id = $event_id;"; ?>
-  <html>
-    <head>
-        <title>Create Activity</title>
-    </head>
-      <h1>Create New Activity</h1>
-    <body>
-      <form action="createactivity.php" method ="POST">
-        Activity Name:
-        <input type="text" name="activity_name" placeholder="Activity name">
-        <br>
-        Scoring Method:
-        <select name="scoring" placeholder="Calculation Method">
-          <option value="placing">Placing</option>
-          <option value="scoring">Scoring</option>
-          <option value="time">Time</option>
-        </select>
-        <br>
-        Unit or Classes:
-        <select name="bracket">
-          <option value="unit">Units</option>
-          <option value="class">Classes</option>
-        </select>
-        <br>
-        If classes selected, specify which classes:
-        <br>
-        <?php
-        while ($row = mysqli_fetch_assoc($result)) {
-            echo "<input type='checkbox' name='class[]' value=" . $row['class_id'];
-            echo ">" . $row['class_name'] . "</input>" . "<br>";
-        } ?>
-        <button type="submit" name="submit">Enter</button>
-      </form>
-    </body>
-  </html>
-  <?php
+$sql = "USE regattascoring;";
+$use = mysqli_query($conn, $sql);
+if (!$use) {
+    echo "Could not use database." . mysqli_error($conn) . "<br/>";
+    exit;
 }
-if (isset($_POST["submit"])) {
-
-  //POST variables from form
-    $activity_name = mysqli_real_escape_string($conn, $_POST['activity_name']);
-    $scoring = mysqli_real_escape_string($conn, $_POST['scoring']);
-    $bracket = mysqli_real_escape_string($conn, $_POST['bracket']);
-    $class = $_POST['class'];
-
-    //array for input sanitsation errors
-    $errors = array();
-
-    //input sanitsation
-    if (!$activity_name) {
-        array_push($errors, "Activity name must be entered");
-    }
-
-    if (preg_match('/[^A-Za-z \-]/', $activity_name)) {
-        array_push($errors, "Please enter a valid activity name");
-    }
-
-    $sql = "SELECT * FROM regattascoring.ACTIVITY;";
-    $result = mysqli_query($conn, $sql);
-    while ($row = mysqli_fetch_assoc($result)) {
-        if ($row['activity_name'] == $activity_name) {
-            array_push($errors, "This activity already exists");
-        }
-    }
-
-    if ($bracket == "unit" and $class != "") {
-        array_push($errors, "Cannot select unit and classes");
-    }
-
-    //select all function from specific table
-    $result = selectall($conn, "class_name", "regattascoring.CLASS", "Class", "activity", "Activities");
-
-    //if errors, echo and exit
-    if (count($errors) != 0) {
-        //call form with existing values?>
-      <html>
-        <head>
-            <title>Create Activity</title>
-        </head>
-        <h1>Create New Activity</h1>
-        <body>
-          <form action="createactivity.php" method ="POST">
-            Activity Name:
-            <input type="text" name="activity_name" value="<?php echo $_POST['activity_name'] ?>" placeholder="Activity name">
-            <br>
-            Scoring Type:
-            <select name="scoring" placeholder="Calculation Method">
-              <option value="placing" <?php if ($_POST['scoring'] == "placing") {
-            echo " selected";
-        } ?>>Placing</option>
-              <option value="scoring"<?php if ($_POST['scoring'] == "scoring") {
-            echo " selected";
-        } ?>>Scoring</option>
-              <option value="time"<?php if ($_POST['scoring'] == "time") {
-            echo " selected";
-        } ?>>Time</option>
-            </select>
-            <br>
-            Unit or Class:
-            <select name="bracket">
-              <option value="unit" <?php if ($_POST['bracket'] == "unit") {
-            echo " selected";
-        } ?>>Units</option>
-              <option value="class" <?php if ($_POST['bracket'] == "class") {
-            echo " selected";
-        } ?>>Classes</option>
-            </select>
-            <br>
-            If classes selected, specify which classes:
-            <br>
-            <?php
-            while ($row = mysqli_fetch_assoc($result)) {
-                echo "<input type='checkbox' name='class[]' value=" . $row['class_id'] . " ";
-                foreach ($class as $match) {
-                    if ($row['class_id'] == $match) {
-                        echo "checked";
-                    }
-                }
-                echo " >" . $row['class_name'] . "</input> <br>";
-            } ?>
-            <br>
-            <button type="submit" name="submit">Enter</button>
-          </form>
-        </body>
-      </html>
-      <?php
-      //echo errors from the input sanitsation
-      $issue = "";
-        foreach ($errors as $error) {
-            $issue = $issue . $error . "</br>";
-        }
-        close($conn, $issue, "activity", "Activities");
-        exit;
-    }
-
-    //Insert variables into activity table, if false echo error and exit
-    $sql = "INSERT INTO regattascoring.ACTIVITY (activity_name, scoring, activity_bracket) VALUES
-    ('$activity_name','$scoring', '$bracket');";
-    if (!mysqli_query($conn, $sql)) {
-        echo mysqli_error($conn);
-        close($conn, "Could not add data", "activity", "Activities");
-        exit;
-    }
-
-    //select most previous id
-    $activity_id = mysqli_insert_id($conn);
-
-    if ($bracket == "class") {
-        foreach ($class as $input) {
-            // add to bracket table
-            $sql = "INSERT INTO regattascoring.BRACKET (activity_id, class_id) VALUES
-            ('$activity_id', '$input');";
-            if (!mysqli_query($conn, $sql)) {
-                echo mysqli_error($conn);
-                close($conn, "Could not add data to bracket table", "activity", "Activities");
-                exit;
-            }
-        }
-    }
-
-    //echo activity created
-    echo $_POST['activity_name'] . " Activity Created"; ?>
-    <br>
-    <a href = <?php echo "viewactivity.php?id=$activity_id>Edit ";
-    echo $_POST['activity_name'] . " Activity</a>";
-
-    //call select all function for form
-    $row = selectall($conn, "class_name", "regattascoring.CLASS", "Class", "activity", "Activities");
-
-    //call activity form
-    activityform($row, $event_id);
-
-    //call closing function
-    close($conn, $error, "activity", "Activities");
-} else {
-
-  //call select all function for form
-    $row = selectall($conn, "class_name", "regattascoring.CLASS", "Class", "activity", "Activities");
-
-    //call activity form
-    activityform($row, $event_id);
-
-    //call close
-    close($conn, $error, "activity", "Activities");
+$sql = "DELETE FROM BRACKET;";
+$delete = mysqli_query($conn, $sql);
+$sql = "DELETE FROM ACTIVITY;";
+$delete = mysqli_query($conn, $sql);
+if (!$delete) {
+    echo "Could not delete all from activity table" . mysqli_error($conn) . "<br/>";
+    exit;
 }
+echo "all in activity table deleted successfully" . "<br/>";
+
+//create all activities
+
+//Cutter Sailing
+$sql = "INSERT INTO ACTIVITY (activity_name, activity_bracket, scoring_method, scored_by) VALUES ('Cutter Sailing', 'class', 'place', 'unit');";
+$result = mysqli_query($conn, $sql);
+$activity_id = mysqli_insert_id($conn);
+$sql = "INSERT INTO BRACKET (class_id, activity_id) VALUES ('2', '$activity_id');";
+$result = mysqli_query($conn, $sql);
+$sql = "INSERT INTO BRACKET (class_id, activity_id) VALUES ('3', '$activity_id');";
+$result = mysqli_query($conn, $sql);
+$sql = "INSERT INTO BRACKET (class_id, activity_id) VALUES ('4', '$activity_id');";
+$result = mysqli_query($conn, $sql);
+
+//Sunburst Sailing
+$sql = "INSERT INTO ACTIVITY (activity_name, activity_bracket, scoring_method, scored_by) VALUES ('Sunburst Sailing', 'class', 'place', 'unit');";
+$result = mysqli_query($conn, $sql);
+$activity_id = mysqli_insert_id($conn);
+$sql = "INSERT INTO BRACKET (class_id, activity_id) VALUES ('2', '$activity_id');";
+$result = mysqli_query($conn, $sql);
+$sql = "INSERT INTO BRACKET (class_id, activity_id) VALUES ('3', '$activity_id');";
+$result = mysqli_query($conn, $sql);
+$sql = "INSERT INTO BRACKET (class_id, activity_id) VALUES ('4', '$activity_id');";
+$result = mysqli_query($conn, $sql);
+
+//Optimist Sailing
+$sql = "INSERT INTO ACTIVITY (activity_name, activity_bracket, scoring_method, scored_by) VALUES ('Optimist Sailing', 'class', 'place', 'unit');";
+$result = mysqli_query($conn, $sql);
+$activity_id = mysqli_insert_id($conn);
+$sql = "INSERT INTO BRACKET (class_id, activity_id) VALUES ('1', '$activity_id');";
+
+//Sunburst Rigging
+$sql = "INSERT INTO ACTIVITY (activity_name, activity_bracket, scoring_method, scored_by) VALUES ('Sunburst Rigging', 'unit', 'score', 'unit');";
+$result = mysqli_query($conn, $sql);
+
+//Cutter Rigging
+$sql = "INSERT INTO ACTIVITY (activity_name, activity_bracket, scoring_method, scored_by) VALUES ('Cutter Rigging', 'unit', 'score', 'unit');";
+$result = mysqli_query($conn, $sql);
+
+//Optimist Rigging
+$sql = "INSERT INTO ACTIVITY (activity_name, activity_bracket, scoring_method, scored_by) VALUES ('Optimist Rigging', 'unit', 'score', 'unit');";
+$result = mysqli_query($conn, $sql);
+
+//Pulling
+$sql = "INSERT INTO ACTIVITY (activity_name, activity_bracket, scoring_method, scored_by) VALUES ('Pulling', 'class', 'place', 'unit');";
+$result = mysqli_query($conn, $sql);
+$activity_id = mysqli_insert_id($conn);
+$sql = "INSERT INTO BRACKET (class_id, activity_id) VALUES ('2', '$activity_id');";
+$result = mysqli_query($conn, $sql);
+$sql = "INSERT INTO BRACKET (class_id, activity_id) VALUES ('3', '$activity_id');";
+$result = mysqli_query($conn, $sql);
+$sql = "INSERT INTO BRACKET (class_id, activity_id) VALUES ('4', '$activity_id');";
+$result = mysqli_query($conn, $sql);
+
+//Canoeing
+$sql = "INSERT INTO ACTIVITY (activity_name, activity_bracket, scoring_method, scored_by) VALUES ('Canoeing', 'class', 'time', 'individual');";
+$result = mysqli_query($conn, $sql);
+$activity_id = mysqli_insert_id($conn);
+$sql = "INSERT INTO BRACKET (class_id, activity_id) VALUES ('1', '$activity_id');";
+$result = mysqli_query($conn, $sql);
+$sql = "INSERT INTO BRACKET (class_id, activity_id) VALUES ('2', '$activity_id');";
+$result = mysqli_query($conn, $sql);
+$sql = "INSERT INTO BRACKET (class_id, activity_id) VALUES ('3', '$activity_id');";
+$result = mysqli_query($conn, $sql);
+$sql = "INSERT INTO BRACKET (class_id, activity_id) VALUES ('4', '$activity_id');";
+$result = mysqli_query($conn, $sql);
+
+//Swimming
+$sql = "INSERT INTO ACTIVITY (activity_name, activity_bracket, scoring_method, scored_by) VALUES ('Swimming', 'class', 'time', 'individual');";
+$result = mysqli_query($conn, $sql);
+$activity_id = mysqli_insert_id($conn);
+$sql = "INSERT INTO BRACKET (class_id, activity_id) VALUES ('1', '$activity_id');";
+$result = mysqli_query($conn, $sql);
+$sql = "INSERT INTO BRACKET (class_id, activity_id) VALUES ('2', '$activity_id');";
+$result = mysqli_query($conn, $sql);
+$sql = "INSERT INTO BRACKET (class_id, activity_id) VALUES ('3', '$activity_id');";
+$result = mysqli_query($conn, $sql);
+$sql = "INSERT INTO BRACKET (class_id, activity_id) VALUES ('4', '$activity_id');";
+$result = mysqli_query($conn, $sql);
+
+//Lifesaving
+$sql = "INSERT INTO ACTIVITY (activity_name, activity_bracket, scoring_method, scored_by) VALUES ('Lifesaving', 'class', 'score', 'unit');";
+$result = mysqli_query($conn, $sql);
+$activity_id = mysqli_insert_id($conn);
+$sql = "INSERT INTO BRACKET (class_id, activity_id) VALUES ('1', '$activity_id');";
+$result = mysqli_query($conn, $sql);
+$sql = "INSERT INTO BRACKET (class_id, activity_id) VALUES ('2', '$activity_id');";
+$result = mysqli_query($conn, $sql);
+$sql = "INSERT INTO BRACKET (class_id, activity_id) VALUES ('3', '$activity_id');";
+$result = mysqli_query($conn, $sql);
+$sql = "INSERT INTO BRACKET (class_id, activity_id) VALUES ('4', '$activity_id');";
+$result = mysqli_query($conn, $sql);
+
+//Shooting
+$sql = "INSERT INTO ACTIVITY (activity_name, activity_bracket, scoring_method, scored_by) VALUES ('Shooting', 'class', 'score', 'individual');";
+$result = mysqli_query($conn, $sql);
+$activity_id = mysqli_insert_id($conn);
+$sql = "INSERT INTO BRACKET (class_id, activity_id) VALUES ('1', '$activity_id');";
+$result = mysqli_query($conn, $sql);
+$sql = "INSERT INTO BRACKET (class_id, activity_id) VALUES ('2', '$activity_id');";
+$result = mysqli_query($conn, $sql);
+$sql = "INSERT INTO BRACKET (class_id, activity_id) VALUES ('3', '$activity_id');";
+$result = mysqli_query($conn, $sql);
+$sql = "INSERT INTO BRACKET (class_id, activity_id) VALUES ('4', '$activity_id');";
+$result = mysqli_query($conn, $sql);
+
+//Camping Set up
+$sql = "INSERT INTO ACTIVITY (activity_name, activity_bracket, scoring_method, scored_by) VALUES ('Camping Set Up', 'unit', 'score', 'unit');";
+$result = mysqli_query($conn, $sql);
+
+//Camping Friday Evening
+$sql = "INSERT INTO ACTIVITY (activity_name, activity_bracket, scoring_method, scored_by) VALUES ('Camping Friday Evening', 'unit', 'score', 'unit');";
+$result = mysqli_query($conn, $sql);
+echo mysqli_error($conn);
+
+//Camping Saturday Morning
+$sql = "INSERT INTO ACTIVITY (activity_name, activity_bracket, scoring_method, scored_by) VALUES ('Camping Saturday Morning', 'unit', 'score', 'unit');";
+$result = mysqli_query($conn, $sql);
+
+//Camping Saturday Evening
+$sql = "INSERT INTO ACTIVITY (activity_name, activity_bracket, scoring_method, scored_by) VALUES ('Camping Saturday Evening', 'unit', 'score', 'unit');";
+$result = mysqli_query($conn, $sql);
+
+//Camping Sunday Morning
+$sql = "INSERT INTO ACTIVITY (activity_name, activity_bracket, scoring_method, scored_by) VALUES ('Camping Sunday Morning', 'unit', 'score', 'unit');";
+$result = mysqli_query($conn, $sql);
+
+//Camping Sunday Evening
+$sql = "INSERT INTO ACTIVITY (activity_name, activity_bracket, scoring_method, scored_by) VALUES ('Camping Sunday Evening', 'unit', 'score', 'unit');";
+$result = mysqli_query($conn, $sql);
+
+//Camping Monday Morning
+$sql = "INSERT INTO ACTIVITY (activity_name, activity_bracket, scoring_method, scored_by) VALUES ('Camping Monday Morning', 'unit', 'score', 'unit');";
+$result = mysqli_query($conn, $sql);
+
+//Iron Woman
+$sql = "INSERT INTO ACTIVITY (activity_name, activity_bracket, scoring_method, scored_by) VALUES ('Iron Woman', 'unit', 'score', 'unit');";
+$result = mysqli_query($conn, $sql);
+//echo classes created
+echo "Classes Created
+</br>
+<a href='viewclass.php'>Edit Classes</a>
+<br>
+<a href='/'>Return Home</a>
+<br>
+<a href='searchclass.php'>View Classes</a>";
