7b3af6febba87a6f314dfc15c89d16b3caed8b0a
Emma Sim-Smith <emmasimsmith@gmail.com>
2019-09-15 13:53:33 +1200

    Created pre set certificates

--- php-apache/src/certificate/createcertificate.php
+++ php-apache/src/certificate/createcertificate.php
@@ -1,149 +1,236 @@
 <?php
 //include navigation bar, functions and connection php files
-include_once '../navbar.php';
 include_once '../connection.php';
-include_once '../functions.php';
 
-//Form function
-function certificateform()
-{
-    ?>
-  <html>
-  <head>
-      <title>Create Certificate</title>
-  </head>
-  <h1>Create New Certificate</h1>
-  <body>
-
-  <form action="createcertificate.php" method ="POST">
-    Certificate Name:
-    <input type="text" name="certificate_name" placeholder="Certificate name">
-    <br>
-    Calculation:
-    <select name="calculation" placeholder="Calculation Method">
-      <option value="selected">Selected</option>
-      <option value="scoring">Scoring</option>
-    </select>
-    <br>
-    Placings:
-    <select name="placing" placeholder="Select Placings">
-      <option value="3">Three Placings</option>
-      <option value="1">One Placing</option>
-    </select>
-    <br>
-    Recipient:
-    <select name="recipient">
-      <option value="individual">Individual</option>
-      <option value="Unit">Unit</option>
-    </select>
-    <br>
-    <button type="submit" name="submit">Enter</button>
-  </form>
-  </body>
-  </html>
-  <?php
+$sql = "USE regattascoring;";
+$use = mysqli_query($conn, $sql);
+if (!$use) {
+    echo "Could not use database." . mysqli_error($conn) . "<br/>";
+    exit;
 }
 
-//if search is submitted
-if (isset($_POST["submit"])) {
-
-    //define POST variables
-    $certificate_name = mysqli_real_escape_string($conn, $_POST['certificate_name']);
-    $calculation = mysqli_real_escape_string($conn, $_POST['calculation']);
-    $placing = mysqli_real_escape_string($conn, $_POST['placing']);
-    $recipient = mysqli_real_escape_string($conn, $_POST['recipient']);
-
-    //array for input sanitsation errors
-    $errors = array();
-
-    //input sanitsation
-    if (!$certificate_name) {
-        array_push($errors, "Certificate name must be entered");
-    }
-    if (preg_match('/[^A-Za-z \-]/', $certificate_name)) {
-        array_push($errors, "Please enter a valid certificate name");
-    }
-
-    //echo errors then exit
-    if (count($errors) != 0) {
-        //call form with existing values?>
-        <html>
-          <form action= "createcertificate.php" method ="POST">
-
-            Certificate Name:
-            <input type="text" name="certificate_name" value="<?php echo $_POST['certificate_name']?>" placeholder="Certificate name">
-            <br>
-
-            Calculation:
-            <select name="calculation" placeholder="Calculation Method">
-              <option value="selected" <?php if ($_POST['calculation'] == "selected") {
-            echo "selected";
-        } ?>  >selected</option>
-              <option value="scoring" <?php if ($_POST['calculation'] == "scoring") {
-            echo "selected";
-        } ?> >Scoring</option>
-            </select>
-            <br>
-
-            Placings:
-            <select name="placing" placeholder="Select Placings">
-              <option value="3" <?php if ($_POST['placing'] == "3") {
-            echo "selected";
-        } ?>>Three Placings</option>
-              <option value="1"<?php if ($_POST['placing'] == "1") {
-            echo "selected";
-        } ?>>One Placing</option>
-            </select>
-            <br>
-
-            Recipient:
-            <select name="recipient">
-              <option value="individual" <?php if ($_POST['recipient'] == "individual") {
-            echo "selected";
-        } ?>>Individual</option>
-              <option value="Unit" <?php if ($_POST['recipient'] == "Unit") {
-            echo "selected";
-        } ?>>Unit</option>
-            </select>
-            <br>
-            <button type="submit" name="submit">Enter</button>
-          </form>
-        </html>
-        <?php
-        //echo errors from the input sanitsation
-        $issue = '';
-        foreach ($errors as $error) {
-            $issue = $issue . $error . "</br>";
-        }
-        close($conn, $issue, "certificate", "Certificates");
-        exit;
-    }
-
-    //Insert variables into certificate table, if false echo error and exit
-    $sql = "INSERT INTO regattascoring.CERTIFICATE (certificate_name, calculation,
-      placing, recipient) VALUES ('$certificate_name','$calculation','$placing', '$recipient');";
-    if (!mysqli_query($conn, $sql)) {
-        close($conn, "Could not add data", "certificate", "Certificates");
-        exit;
-    }
-
-    //echo certificate created
-    echo $_POST['certificate_name'] . " Certificate Created";
-    $certificate_id = mysqli_insert_id($conn); ?>
-    <br>
-    <a href = <?php echo "viewcertificate.php?id=$certificate_id"?>>Edit
-      <?php echo $_POST['certificate_name'] . " Certificate" ?></a>
-    <?php
-
-    //call certificate form
-    certificateform();
-
-    //call close
-    close($conn, $error, "certificate", "Certificates");
-} else {
-    //call certificate form
-    certificateform();
-
-    //call close
-    close($conn, $error, "certificate", "Certificates");
+$sql = "DELETE FROM CERTIFICATE;";
+$delete = mysqli_query($conn, $sql);
+if (!$delete) {
+    echo "Could not delete all from certificate table" . mysqli_error($conn) . "<br/>";
+    exit;
 }
+echo "deleted successfully" . "<br/>";
+
+//create all certificates
+
+//Camping
+$sql = "INSERT INTO CERTIFICATE (certificate_name, placing, recipient) VALUES ('Camping', '3', 'unit');";
+$result = mysqli_query($conn, $sql);
+
+//JJ Canoeing
+$sql = "INSERT INTO CERTIFICATE (certificate_name, placing, recipient) VALUES ('JJ Canoeing', '3', 'unit');";
+$result = mysqli_query($conn, $sql);
+
+//Junior Canoeing
+$sql = "INSERT INTO CERTIFICATE (certificate_name, placing, recipient) VALUES ('Junior Canoeing', '3', 'unit');";
+$result = mysqli_query($conn, $sql);
+
+//Intermediate Canoeing
+$sql = "INSERT INTO CERTIFICATE (certificate_name, placing, recipient) VALUES ('Intermediate Canoeing', '3', 'unit');";
+$result = mysqli_query($conn, $sql);
+
+//Senior Canoeing
+$sql = "INSERT INTO CERTIFICATE (certificate_name, placing, recipient) VALUES ('Senior Canoeing', '3', 'unit');";
+$result = mysqli_query($conn, $sql);
+
+//Open Canoeing
+$sql = "INSERT INTO CERTIFICATE (certificate_name, placing, recipient) VALUES ('Open Canoeing', '3', 'unit');";
+$result = mysqli_query($conn, $sql);
+
+//Overall Canoeing
+$sql = "INSERT INTO CERTIFICATE (certificate_name, placing, recipient) VALUES ('Overall Canoeing', '3', 'unit');";
+$result = mysqli_query($conn, $sql);
+
+//Participation
+$sql = "INSERT INTO CERTIFICATE (certificate_name, placing, recipient) VALUES ('Participation', '3', 'unit');";
+$result = mysqli_query($conn, $sql);
+
+//JJ Lifesaving
+$sql = "INSERT INTO CERTIFICATE (certificate_name, placing, recipient) VALUES ('JJ Lifesaving', '3', 'unit');";
+$result = mysqli_query($conn, $sql);
+
+//Junior Lifesaving
+$sql = "INSERT INTO CERTIFICATE (certificate_name, placing, recipient) VALUES ('Junior Lifesaving', '3', 'unit');";
+$result = mysqli_query($conn, $sql);
+
+//Intermediate Lifesaving
+$sql = "INSERT INTO CERTIFICATE (certificate_name, placing, recipient) VALUES ('Intermediate Lifesaving', '3', 'unit');";
+$result = mysqli_query($conn, $sql);
+
+//Senior Lifesaving
+$sql = "INSERT INTO CERTIFICATE (certificate_name, placing, recipient) VALUES ('Senior Lifesaving', '3', 'unit');";
+$result = mysqli_query($conn, $sql);
+
+//Overall Lifesaving
+$sql = "INSERT INTO CERTIFICATE (certificate_name, placing, recipient) VALUES ('Overall Lifesaving', '3', 'unit');";
+$result = mysqli_query($conn, $sql);
+
+//Junior Pulling
+$sql = "INSERT INTO CERTIFICATE (certificate_name, placing, recipient) VALUES ('Junior Pulling', '3', 'unit');";
+$result = mysqli_query($conn, $sql);
+
+//Intermediate Pulling
+$sql = "INSERT INTO CERTIFICATE (certificate_name, placing, recipient) VALUES ('Intermediate Pulling', '3', 'unit');";
+$result = mysqli_query($conn, $sql);
+
+//Senior Pulling
+$sql = "INSERT INTO CERTIFICATE (certificate_name, placing, recipient) VALUES ('Senior Pulling', '3', 'unit');";
+$result = mysqli_query($conn, $sql);
+
+//Open Pulling
+$sql = "INSERT INTO CERTIFICATE (certificate_name, placing, recipient) VALUES ('Open Pulling', '3', 'unit');";
+$result = mysqli_query($conn, $sql);
+
+//Overall Pulling
+$sql = "INSERT INTO CERTIFICATE (certificate_name, placing, recipient) VALUES ('Overall Pulling', '3', 'unit');";
+$result = mysqli_query($conn, $sql);
+
+//Cutter Rigging
+$sql = "INSERT INTO CERTIFICATE (certificate_name, placing, recipient) VALUES ('Cutter Rigging', '3', 'unit');";
+$result = mysqli_query($conn, $sql);
+
+//Sunburst Rigging
+$sql = "INSERT INTO CERTIFICATE (certificate_name, placing, recipient) VALUES ('Sunburst Rigging', '3', 'unit');";
+$result = mysqli_query($conn, $sql);
+
+//Optimist Rigging
+$sql = "INSERT INTO CERTIFICATE (certificate_name, placing, recipient) VALUES ('Optimist Rigging', '3', 'unit');";
+$result = mysqli_query($conn, $sql);
+
+//Junior Cutter Sailing
+$sql = "INSERT INTO CERTIFICATE (certificate_name, placing, recipient) VALUES ('Junior Cutter Sailing', '3', 'unit');";
+$result = mysqli_query($conn, $sql);
+
+//Intermediate Cutter Sailing
+$sql = "INSERT INTO CERTIFICATE (certificate_name, placing, recipient) VALUES ('Intermediate Cutter Sailing', '3', 'unit');";
+$result = mysqli_query($conn, $sql);
+
+//Senior Cutter Sailing
+$sql = "INSERT INTO CERTIFICATE (certificate_name, placing, recipient) VALUES ('Senior Cutter Sailing', '3', 'unit');";
+$result = mysqli_query($conn, $sql);
+
+//Open Cutter Sailing
+$sql = "INSERT INTO CERTIFICATE (certificate_name, placing, recipient) VALUES ('Open Cutter Sailing', '3', 'unit');";
+$result = mysqli_query($conn, $sql);
+
+//Junior Sunburst Sailing
+$sql = "INSERT INTO CERTIFICATE (certificate_name, placing, recipient) VALUES ('Junior Sunburst Sailing', '3', 'unit');";
+$result = mysqli_query($conn, $sql);
+
+//Intermediate Sunburst Sailing
+$sql = "INSERT INTO CERTIFICATE (certificate_name, placing, recipient) VALUES ('Intermediate Sunburst Sailing', '3', 'unit');";
+$result = mysqli_query($conn, $sql);
+
+//Senior Sunburst Sailing
+$sql = "INSERT INTO CERTIFICATE (certificate_name, placing, recipient) VALUES ('Senior Sunburst Sailing', '3', 'unit');";
+$result = mysqli_query($conn, $sql);
+
+//Open Sunburst Sailing
+$sql = "INSERT INTO CERTIFICATE (certificate_name, placing, recipient) VALUES ('Open Sunburst Sailing', '3', 'unit');";
+$result = mysqli_query($conn, $sql);
+
+//JJ Optimist Sailing
+$sql = "INSERT INTO CERTIFICATE (certificate_name, placing, recipient) VALUES ('JJ Optimist Sailing', '3', 'unit');";
+$result = mysqli_query($conn, $sql);
+
+//Overall Junior Sailing
+$sql = "INSERT INTO CERTIFICATE (certificate_name, placing, recipient) VALUES ('Overall Junior Sailing', '3', 'unit');";
+$result = mysqli_query($conn, $sql);
+
+//Overall Intermediate Sailing
+$sql = "INSERT INTO CERTIFICATE (certificate_name, placing, recipient) VALUES ('Overall Intermediate Sailing', '3', 'unit');";
+$result = mysqli_query($conn, $sql);
+
+//Overall Senior Sailing
+$sql = "INSERT INTO CERTIFICATE (certificate_name, placing, recipient) VALUES ('Overall Senior Sailing', '3', 'unit');";
+$result = mysqli_query($conn, $sql);
+
+//Overall Open Sailing
+$sql = "INSERT INTO CERTIFICATE (certificate_name, placing, recipient) VALUES ('Overall Open Sailing', '3', 'unit');";
+$result = mysqli_query($conn, $sql);
+
+//Overall Sailing
+$sql = "INSERT INTO CERTIFICATE (certificate_name, placing, recipient) VALUES ('Overall Sailing', '3', 'unit');";
+$result = mysqli_query($conn, $sql);
+
+//Seamanship
+$sql = "INSERT INTO CERTIFICATE (certificate_name, placing, recipient) VALUES ('Seamanship', '3', 'unit');";
+$result = mysqli_query($conn, $sql);
+
+//Ron Bird Seamanship
+$sql = "INSERT INTO CERTIFICATE (certificate_name, placing, recipient) VALUES ('Ron Bird Seamanship', '3', 'unit');";
+$result = mysqli_query($conn, $sql);
+
+//Boat Handling
+$sql = "INSERT INTO CERTIFICATE (certificate_name, placing, recipient) VALUES ('Boat Handling', '3', 'unit');";
+$result = mysqli_query($conn, $sql);
+
+//Iron Woman
+$sql = "INSERT INTO CERTIFICATE (certificate_name, placing, recipient) VALUES ('Iron Woman', '3', 'unit');";
+$result = mysqli_query($conn, $sql);
+
+//JJ Swimming
+$sql = "INSERT INTO CERTIFICATE (certificate_name, placing, recipient) VALUES ('JJ Swimming', '3', 'unit');";
+$result = mysqli_query($conn, $sql);
+
+//Junior Swimming
+$sql = "INSERT INTO CERTIFICATE (certificate_name, placing, recipient) VALUES ('Junior Swimming', '3', 'unit');";
+$result = mysqli_query($conn, $sql);
+
+//Intermediate Swimming
+$sql = "INSERT INTO CERTIFICATE (certificate_name, placing, recipient) VALUES ('Intermediate Swimming', '3', 'unit');";
+$result = mysqli_query($conn, $sql);
+
+//Senior Swimming
+$sql = "INSERT INTO CERTIFICATE (certificate_name, placing, recipient) VALUES ('Senior Swimming', '3', 'unit');";
+$result = mysqli_query($conn, $sql);
+
+//Open Swimming
+$sql = "INSERT INTO CERTIFICATE (certificate_name, placing, recipient) VALUES ('Open Swimming', '3', 'unit');";
+$result = mysqli_query($conn, $sql);
+
+//Overall Swimming
+$sql = "INSERT INTO CERTIFICATE (certificate_name, placing, recipient) VALUES ('Overall Swimming', '3', 'unit');";
+$result = mysqli_query($conn, $sql);
+
+//JJ Shooting
+$sql = "INSERT INTO CERTIFICATE (certificate_name, placing, recipient) VALUES ('JJ Shooting', '3', 'unit');";
+$result = mysqli_query($conn, $sql);
+
+//Junior Shooting
+$sql = "INSERT INTO CERTIFICATE (certificate_name, placing, recipient) VALUES ('Junior Shooting', '3', 'unit');";
+$result = mysqli_query($conn, $sql);
+
+//Intermediate Shooting
+$sql = "INSERT INTO CERTIFICATE (certificate_name, placing, recipient) VALUES ('Intermediate Shooting', '3', 'unit');";
+$result = mysqli_query($conn, $sql);
+
+//Senior Shooting
+$sql = "INSERT INTO CERTIFICATE (certificate_name, placing, recipient) VALUES ('Senior Shooting', '3', 'unit');";
+$result = mysqli_query($conn, $sql);
+
+//Overall Shooting
+$sql = "INSERT INTO CERTIFICATE (certificate_name, placing, recipient) VALUES ('Overall Shooting', '3', 'unit');";
+$result = mysqli_query($conn, $sql);
+
+//Friendship
+$sql = "INSERT INTO CERTIFICATE (certificate_name, placing, recipient) VALUES ('Friendship', '3', 'unit');";
+$result = mysqli_query($conn, $sql);
+
+//Outstanding Mariner
+$sql = "INSERT INTO CERTIFICATE (certificate_name, placing, recipient) VALUES ('Outstanding Mariner', '1', 'unit');";
+$result = mysqli_query($conn, $sql);
+
+//Leading Mariner
+$sql = "INSERT INTO CERTIFICATE (certificate_name, placing, recipient) VALUES ('Leading Mariner', '1', 'unit');";
+$result = mysqli_query($conn, $sql);
+
+//Outstanding Mariner Runner Up
+$sql = "INSERT INTO CERTIFICATE (certificate_name, placing, recipient) VALUES ('Outstanding Mariner Runner Up', '1', 'unit');";
+$result = mysqli_query($conn, $sql);
