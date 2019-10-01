598b5ffa7e1db597245656103f2df1c18a76c98d
Emma Sim-Smith <emmasimsmith@gmail.com>
2019-10-01 18:32:07 +1300

    fixed smallerror

--- php-apache/src/race_enrolment/createrace_enrolment.php
+++ php-apache/src/race_enrolment/createrace_enrolment.php
@@ -16,7 +16,7 @@ if (!$result) {
     home_close($conn);
 }
 if (mysqli_num_rows($result) == 0) {
-    echo "please create classes first";
+    echo "please create activities first";
     home_close($conn);
 }
 //create dropdown form
