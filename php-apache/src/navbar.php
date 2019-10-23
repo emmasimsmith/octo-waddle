aa083512979a386e14068f803cd148234bcfae18
Emma Sim-Smith <emmasimsmith@gmail.com>
2019-10-23 20:42:43 +1300

    Designed and Styled nav bar

--- php-apache/src/navbar.php
+++ php-apache/src/navbar.php
@@ -1,17 +1,74 @@
-<div class="navbar">
-  <ul>
-    <li><a href="/">Home</a></li>
-    <li><a href="/unit/createunit.php">Create Unit</a></li>
-    <li><a href="/unit/searchunit.php">View Units</a></li>
-    <li><a href="/individual/createindividual.php">Create Individual</a></li>
-    <li><a href="/individual/searchindividual.php">View Individuals</a></li>
-    <li><a href="/class/createclass.php">Create Class</a></li>
-    <li><a href="/class/searchclass.php">View Classes</a></li>
-    <li><a href="/boat/createboat.php">Create Boat</a></li>
-    <li><a href="/boat/searchboat.php">View Boat</a></li>
-    <li><a href="/event/createevent.php">Create Event</a></li>
-    <li><a href="/event/searchevent.php">View Events</a></li>
-    <li><a href="/certificate/searchcertificate.php">View Certificates</a></li>
-    <li><a href="/activity/searchactivity.php">View Activities</a></li>
-  </ul>
-</div>
+    <nav class="nav-main">
+      <div class="logo">Young Mariners Regatta Scoring</div>
+      <ul>
+        <li>
+          <a href="/" class="nav-item">Home</a>
+        </li>
+        <li>
+          <a href="#" class="nav-item">Events</a>
+          <div class="nav-content">
+            <div class="nav-sub">
+              <ul>
+                <li><a href="/event/createevent.php">Create Event</a></li>
+                <li><a href="/event/searchevent.php">View Events</a></li>
+              </ul>
+            </div>
+          </div>
+        </li>
+        <li>
+          <a href="#" class="nav-item">Units</a>
+          <div class="nav-content">
+            <div class="nav-sub">
+              <ul>
+                <li><a href="/unit/createunit.php">Create Unit</a></li>
+                <li><a href="/unit/searchunit.php">View Units</a></li>
+              </ul>
+            </div>
+          </div>
+        </li>
+        <li>
+          <a href="#" class="nav-item">Classes</a>
+          <div class="nav-content">
+            <div class="nav-sub">
+              <ul>
+                <li><a href="/event/createclass.php">Create Classes</a></li>
+                <li><a href="/event/searchclasses.php">View Classes</a></li>
+              </ul>
+            </div>
+          </div>
+        </li>
+        <li>
+          <a href="#" class="nav-item">Individuals</a>
+          <div class="nav-content">
+            <div class="nav-sub">
+              <ul>
+                <li><a href="/individual/createindividual.php">Create Individual</a></li>
+                <li><a href="/individual/searchindividual.php">View Individuals</a></li>
+              </ul>
+            </div>
+          </div>
+        </li>
+        <li>
+          <a href="#" class="nav-item">Boats</a>
+          <div class="nav-content">
+            <div class="nav-sub">
+              <ul>
+                <li><a href="/boat/createboat.php">Create Boat</a></li>
+                <li><a href="/boat/searchboat.php">View Boats</a></li>
+              </ul>
+            </div>
+          </div>
+        </li>
+        <li>
+          <a href="#" class="nav-item">Other</a>
+          <div class="nav-content">
+            <div class="nav-sub">
+              <ul>
+                <li><a href="/certificate/searchcertificate.php">View Certificates</a></li>
+                <li><a href="/activity/searchactivity.php">View Activities</a></li>
+              </ul>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </nav>
