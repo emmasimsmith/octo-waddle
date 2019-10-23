b06cc88014e223035f9f39c6b362b193a27aeb59
Emma Sim-Smith <emmasimsmith@gmail.com>
2019-10-23 20:43:25 +1300

    Index edited and styled

--- php-apache/src/index.php
+++ php-apache/src/index.php
@@ -4,14 +4,37 @@ include_once 'connection.php';
 
 ?>
 <html>
-<head>
-    <title>Octo Waddle</title>
-</head>
-<body>
-
-<h1>Home</h1>
-<a href="indexevents.php">Events</a>
-<br>
-
-</body>
+  <head>
+    <title>Regatta Scoring</title>
+    <link rel="stylesheet" type="text/css" href="stylesheets/navbarstyle.css">
+    <link rel="stylesheet" type="text/css" href="stylesheets/indexstyle.css">
+  </head>
+  <body>
+    <div class="outer-container">
+      <div class="container">
+        <div class="content">
+          <h1>Welcome to Young Mariners Regatta Scoring System</h1>
+          <p>
+            This website enables regatta scorers an easy accessible method to input
+            and calculate results. No longer do you have to worry about inefficient
+            or complicated systems, use Young Mariners Regatta Scoring System for
+            a breezy day of scoring.
+          </p>
+          <h2>Getting Started</h2>
+          <p1>
+            If you have not already started, first create an
+            <a href="event/createevent.php">Event</a>
+          </p1>
+          <h2>Already Started?</h2>
+          <p1>Great! Continue by creating or viewing your information through the navigation
+            bar, or
+            <a href="indexevents.php">Select an Event</a>
+        <br>
+        </div>
+      </div>
+    </div>
+    <div class="bottom-bar">
+      <h1>Emma Sim-Smith &copy; 2019</h1>
+    </div>
+  </body>
 </html>
