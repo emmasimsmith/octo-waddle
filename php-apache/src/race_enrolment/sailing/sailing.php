3e414fa9813d838b470fc181ce169d726c2923d1
Emma Sim-Smith <emmasimsmith@gmail.com>
2019-11-01 10:54:12 +1300

    Input into sailing table to allow editing

--- php-apache/src/race_enrolment/sailing/sailing.php
+++ php-apache/src/race_enrolment/sailing/sailing.php
@@ -42,10 +42,10 @@ if (isset($_POST['submit'])) {
             array_push($errors, "Enter a score for $boat_number $boat_type");
         }
         if ($_POST["$boat_id"] == "DNC" and $_POST["$boat_number"]) {
-            array_push($errors, "Cannot select DNC and enter a score for $boat_number");
+            array_push($errors, "Cannot select DNC and enter a score for $boat_number $boat_type");
         }
         if ($_POST["$boat_id"] == "DNF" and $_POST["$boat_number"]) {
-            array_push($errors, "Cannot select DNF and enter a score for $boat_number");
+            array_push($errors, "Cannot select DNF and enter a score for $boat_number $boat_type");
         }
     }
     if (count($errors) != 0) {
@@ -88,21 +88,23 @@ if (isset($_POST['submit'])) {
 
                     echo $boat_num . " Score: ";
                     echo "<input type='number' name='$boat_num' placeholder='Score' min=0 max=5000 value =" . $_POST["$boat_num"] . ">";
-                    echo "<input type ='radio' name='$boat_id' value='completed'";
+                    echo "<select name='$boat_id'>
+                    <option value='completed'";
                     if ($_POST["$boat_id"] == "completed") {
-                        echo " checked";
+                        echo " selected";
                     }
-                    echo "> completed </input>";
-                    echo "<input type ='radio' name='$boat_id' value='DNC'" ;
+                    echo "> completed </option>";
+                    echo "<option value='DNC'" ;
                     if ($_POST["$boat_id"] == "DNC") {
-                        echo " checked";
+                        echo " selected";
                     }
-                    echo "> DNC </input>";
-                    echo "<input type ='radio' name='$boat_id' value='DNF'";
+                    echo "> DNC </option>";
+                    echo "<option value='DNF'";
                     if ($_POST["$boat_id"] == "DNF") {
-                        echo " checked";
+                        echo " selected";
                     }
-                    echo "> DNF </input>";
+                    echo "> DNF </option>
+                    </select>";
                     echo "<br>";
                 }
             }
@@ -172,8 +174,10 @@ if (isset($_POST['submit'])) {
                 array_push($unit_array, $final_time);
             }
         }
-        $add = min($unit_array);
-        $sort += [$unit_id => $add];
+        if ($unit_array) {
+            $add = min($unit_array);
+            $sort += [$unit_id => $add];
+        }
     }
 
     //sort array in descending order
@@ -212,7 +216,7 @@ if (isset($_POST['submit'])) {
 
         //check there is not already an input for the unit
         $sql = "SELECT * FROM regattascoring.RACE_ENROLMENT WHERE event_id = '$event_id'
-      AND activity_id = '$activity_id' AND race_number = '$race_number' AND unit_id = '$unit_id';";
+      AND activity_id = '$activity_id' AND race_number = '$race_number' AND unit_id = '$unit_id' and class_id ='$class_id';";
         $check = mysqli_query($conn, $sql);
 
         //if not
@@ -229,7 +233,7 @@ if (isset($_POST['submit'])) {
                     //set score
                     $placescore = $base-1;
                     //insert value into table
-                    input($conn, $activity_id, $unit_id, 'DNF', $placescore, 'NULL', $event_id, $race_number);
+                    input($conn, $activity_id, $unit_id, $class_id, 'DNF', $placescore, 'NULL', $event_id, $race_number);
                 }
             }
         }
@@ -243,17 +247,19 @@ if (isset($_POST['submit'])) {
         //define unit id
         $unit_id = $completed['unit_id'];
 
-        //check there is not already an input for the unit
-        $sql = "SELECT * FROM regattascoring.RACE_ENROLMENT WHERE event_id = '$event_id'
-      AND activity_id = '$activity_id' AND race_number = '$race_number' AND unit_id = '$unit_id';";
-        $check = mysqli_query($conn, $sql);
+        //select boats from unit
+        $sql = "SELECT * FROM regattascoring.BOAT WHERE boat_type='$boat_type' AND unit_id = '$unit_id';";
+        $boat_result = mysqli_query($conn, $sql);
+        while ($boat_completed = mysqli_fetch_assoc($boat_result)) {
 
-        //if not
-        if (mysqli_num_rows($check) == 0) {
-            //select boats from unit
-            $sql = "SELECT * FROM regattascoring.BOAT WHERE boat_type='$boat_type' AND unit_id = '$unit_id';";
-            $boat_result = mysqli_query($conn, $sql);
-            while ($boat_completed = mysqli_fetch_assoc($boat_result)) {
+              //check there is not already an input for the unit
+            $sql = "SELECT * FROM regattascoring.RACE_ENROLMENT WHERE event_id = '$event_id'
+            AND activity_id = '$activity_id' AND race_number = '$race_number' AND unit_id = '$unit_id' and class_id = '$class_id';";
+            $check = mysqli_query($conn, $sql);
+
+            //if not
+            if (mysqli_num_rows($check) == 0) {
+
                 //define boat id
                 $boat_id = $boat_completed['boat_id'];
 
@@ -262,17 +268,50 @@ if (isset($_POST['submit'])) {
                     //set score
                     $placescore = $base-2;
                     //insert value into table
-                    input($conn, $activity_id, $unit_id, 'DNC', $placescore, 'NULL', $event_id, $race_number);
+                    input($conn, $activity_id, $unit_id, $class_id, 'DNC', $placescore, 'NULL', $event_id, $race_number);
                 }
             }
         }
     }
     //check if any teams are tied
     tied($conn, $event_id, $activity_id, $class_id, $race_number);
-    
+
+    //add all results to sailing table to edit
+    $sql ="SELECT * FROM regattascoring.UNIT;";
+    $unit_result = mysqli_query($conn, $sql);
+    while ($unit_row = mysqli_fetch_assoc($unit_result)) {
+        echo $unit_row['unit_name'] .'<br>';
+        //select unit id for the where statement and input
+        $unit_id = $unit_row['unit_id'];
+        $sql = "SELECT * FROM regattascoring.BOAT WHERE unit_id = $unit_id and boat_type = '$boat_type';";
+        $boat_result = mysqli_query($conn, $sql);
+        while ($boat_row = mysqli_fetch_assoc($boat_result)) {
+            //define boat id, boat number, time and result for input into sailing table
+            $boat_id = $boat_row['boat_id'];
+            $boat_number = $boat_row['boat_number'];
+            $sailing_time = $_POST["$boat_number"];
+            if (!$sailing_time) {
+                $sailing_time = 'NULL';
+            }
+            $sailing_result = $_POST["$boat_id"];
+            echo "sailing time: $sailing_time";
+            echo " sailing result: $sailing_result";
+            //insert boat into regattascoring
+            $sql = "INSERT INTO regattascoring.SAILING (activity_id, unit_id, boat_id,
+          sailing_time, sailing_result) VALUES ('$activity_id', '$unit_id', '$boat_id',
+          $sailing_time, '$sailing_result');";
+            $input = mysqli_query($conn, $sql);
+            if (!$input) {
+                echo "Could not add boats to sailing table<br>" . mysqli_error($conn);
+                mysqli_close($conn);
+                exit;
+            }
+        }
+    }
+
     echo "Race results added";
     echo "<br>
-    <a href=editsailing.php?event_id=$event_id&activity_id=$activity_id&class_id=$class_id>Edit Activity</a>
+    <a href=editsailing.php?event_id=$event_id&activity_id=$activity_id&class_id=$class_id&race_number=$race_number&boat_type=$boat_type>Edit Activity</a>
     <br>
     <a href='/'>Return Home</a>
     <br>
@@ -328,9 +367,11 @@ if (isset($_POST['submit'])) {
             while ($boat_row = mysqli_fetch_assoc($boats)) {
                 echo $boat_row['boat_number'] . " Score: ";
                 echo "<input type='number' name='" . $boat_row['boat_number']. "' placeholder='Score' min=0 max=5000 >";
-                echo "<input type ='radio' name='" . $boat_row['boat_id'] . "' value='completed'> completed </input>";
-                echo "<input type ='radio' name='" . $boat_row['boat_id'] . "' value='DNC'> DNC </input>";
-                echo "<input type ='radio' name='" . $boat_row['boat_id'] . "' value='DNF'> DNF </input>";
+                echo "<select name='" . $boat_row['boat_id'] . "'>
+                <option value='completed'>completed</option>
+                <option value='DNC'>DNC</option>
+                <option value='DNF'>DNF</option>
+                </select>";
                 echo "<br>";
             }
         }
