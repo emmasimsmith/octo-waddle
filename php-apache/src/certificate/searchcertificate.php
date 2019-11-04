b240bc05ae454b41b68007c2336c2d346862b753
Emma Sim-Smith <emmasimsmith@gmail.com>
2019-11-04 13:24:46 +1300

    Fixed and added CSS for certificate view page

--- php-apache/src/certificate/searchcertificate.php
+++ php-apache/src/certificate/searchcertificate.php
@@ -1,3 +1,10 @@
+<html>
+  <head>
+    <title>View Certificates</title>
+    <link rel="stylesheet" type="text/css" href="../stylesheets/navbarstyle.css">
+    <link rel="stylesheet" type="text/css" href="../stylesheets/pagestyle.css">
+  </head>
+
 <?php
 //include the navigation bar and connection php files
 include_once '../navbar.php';
@@ -9,16 +16,22 @@ include_once '../morefunctions.php';
 function certificateform()
 {
     ?>
-  <html>
-    <body>
+    <h1>View Certificates</h1>
+    <div class="search_form">
       <form action= searchcertificate.php method="POST">
-      <input type="text" name="certificate_name" placeholder="Search certificate name">
-      <input type="number" name="placing" placeholder="Search number of placings">
-      <input type="text" name="recipient" placeholder="Search recipients">
-      <button type="submit" name="search">Enter</button>
+        <div class="form_input">
+          <div class="three_input">
+            <input type="text" name="certificate_name" placeholder="Certificate Name">
+            <input type="number" name="placing" placeholder="Number of Placings">
+            <input type="text" name="recipient" placeholder="Recipients">
+          </div>
+        </div>
+        <div class="search_button">
+          <button type="submit" name="search">Enter</button>
+        </div>
       </form>
-    </body>
-  </html>
+    </div>
+  </body>
   <?php
 }
 
@@ -26,6 +39,9 @@ function certificateform()
 if (isset($_POST['search'])) {
 
     //call form again
+    echo "  <div class='container'>
+        <div class='content'>
+          <body>";
     certificateform();
 
     //define searched variables
@@ -35,11 +51,14 @@ if (isset($_POST['search'])) {
 
     //validation check incase strings are empty
     if (!$_POST['certificate_name'] and !$_POST['placing'] and !$_POST['recipient']) {
-        echo "Please search a valid value
-      <br>
-      <a href='/'>Return Home</a>
-      <br>
-      <a href='searchcertificate.php'>View all Certificates</a>";
+        echo "
+        <div class='error'>Please search a valid value</div>
+        <div class='close'>
+          <ul>
+            <li><a href='/'>Return Home</a></li>
+            <li><a href='searchcertificate.php'>View all Certificates</a></li>
+          </ul>
+        </div>";
         mysqli_close($conn);
         exit;
     }
@@ -63,11 +82,14 @@ if (isset($_POST['search'])) {
     //check if there are rows that match
     $search = mysqli_query($conn, $sql);
     if (mysqli_num_rows($search) == 0) {
-        echo "No matches in table" . "</br>";
-        echo "<br>
-        <a href='/'>Return Home</a>
-        <br>
-        <a href='searchcertificate.php'>View all Certificates</a>";
+        echo "
+      <div class='message'>No matches in table</div>
+      <div class='close'>
+        <ul>
+          <li><a href='/'>Return Home</a></li>
+          <li><a href='searchcertificate.php'>View all Certificates</a></li>
+        </ul>
+      </div>";
         mysqli_close($conn);
         exit;
     }
@@ -76,12 +98,18 @@ if (isset($_POST['search'])) {
     certificate_view($conn, $search);
 
     //close
-    echo "<br>
-    <a href='/'>Return Home</a>
-    <br>
-    <a href='searchcertificate.php'>View all Certificates</a>";
+    echo "
+    <div class='close'>
+      <ul>
+        <li><a href='/'>Return Home</a></li>
+        <li><a href='searchcertificate.php'>View all Certificates</a></li>
+      </ul>
+    </div>";
 } else {
     //call certificate form
+    echo "  <div class='container'>
+        <div class='content'>
+          <body>";
     certificateform();
 
     //echo all data from table
@@ -89,4 +117,10 @@ if (isset($_POST['search'])) {
     $result = mysqli_query($conn, $sql);
 
     certificate_view($conn, $result);
+    echo"
+    <div class='close'>
+      <ul>
+        <li><a href='/'>Return Home</a></li>
+      </ul>
+    </div>";
 }
