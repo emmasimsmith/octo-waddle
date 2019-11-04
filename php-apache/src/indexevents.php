225a391e40bd4147561a0306b7ce79cdf8672bbf
Emma Sim-Smith <emmasimsmith@gmail.com>
2019-11-04 13:24:15 +1300

    Fixed and added CSS for selecting event page

--- php-apache/src/indexevents.php
+++ php-apache/src/indexevents.php
@@ -1,26 +1,49 @@
+<html>
+  <head>
+    <title>Select Event</title>
+    <link rel="stylesheet" type="text/css" href="../stylesheets/navbarstyle.css">
+    <link rel="stylesheet" type="text/css" href="../stylesheets/pagestyle.css">
+  </head>
+
 <?php
 include_once 'navbar.php';
 include_once 'connection.php';
 
 ?>
-<html>
-  <head>
-      <title>Octo Waddle</title>
-  </head>
+<div class='container'>
+  <div class='content'>
+    <body>
       <h1>Events</h1>
-  <body>
-    <?php
-    //select all from event table
-    $sql = "SELECT * FROM regattascoring.EVENT;";
-    $result = mysqli_query($conn, $sql);
-    if (mysqli_num_rows($result) == 0) {
-        echo "No events created <br><br>";
-        echo "<a href=/event/createevent.php>Create Event</a>";
-        echo "<a href='/'>Return Home</a>";
-    }
-    while ($row = mysqli_fetch_assoc($result)) {
-        echo "<a href=indexselectedevent.php?event_id=" . $row['event_id'] . ">" . $row['location'] . " Event</a>";
-        echo "<br>";
-    } ?>
-  </body>
-</html>
+      <div class="event_list">
+        <ul>
+        <?php
+        //select all from event table
+        $sql = "SELECT * FROM regattascoring.EVENT;";
+        $result = mysqli_query($conn, $sql);
+        if (mysqli_num_rows($result) == 0) {
+            echo "<div class='message'>No events created</div>
+            <div class='close'>
+              <ul>
+                <li><a href=/event/createevent.php>Create Event</a></li>
+                <li><a href='/'>Return Home</a></li>
+              </ul>
+              </div>
+              </div>
+              </div>";
+            mysqli_close($conn);
+            exit;
+        }
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo "<li><a href=indexselectedevent.php?event_id=" . $row['event_id'] . ">" . $row['location'] . " Event ". $row['event_date']."</a></li>";
+        } ?>
+        </ul>
+      </div>
+    </body>
+    <div class='close'>
+      <ul>
+        <li><a href=/event/createevent.php>Create Event</a></li>
+        <li><a href='/'>Return Home</a></li>
+      </ul>
+    </div>
+  </div>
+</div>
--- php-apache/src/indexselectedevent.php
+++ php-apache/src/indexselectedevent.php
@@ -8,22 +8,34 @@ $event_id = $_GET['event_id'];
 $sql = "SELECT * FROM regattascoring.EVENT WHERE event_id = $event_id;";
 $result = mysqli_query($conn, $sql);
 $row = mysqli_fetch_assoc($result);
-
-?>
+$location = $row['location'];?>
 <html>
   <head>
-      <title>Octo Waddle</title>
+    <title><?php echo $location?> Event</title>
+    <link rel="stylesheet" type="text/css" href="../stylesheets/navbarstyle.css">
+    <link rel="stylesheet" type="text/css" href="../stylesheets/pagestyle.css">
   </head>
-      <h1> <?php echo $row['location'] ?> Event</h1>
-  <body>
-    <?php
-    echo
-    "<ul>
-      <li><a href=/participant/selectparticipant.php?event_id=$event_id>Select Participants</a></li>
-      <li><a href=/participant/searchparticipant.php?event_id=$event_id>View all Participants</a></li>
-      <li><a href=/race_enrolment/enrolment.php?event_id=$event_id>Enroll Participants</a></li>
-      <li><a href=/award/calculateawards.php?event_id=$event_id>Calculate Awards</a></li>
-    </ul>";
+  <div class='container'>
+    <div class='content'>
+      <body>
+        <h1>Events</h1>
+        <div class="event_list">
+        <?php
+        echo
+        "<ul>
+          <li><a href=/participant/selectparticipant.php?event_id=$event_id>Select Participants</a></li>
+          <li><a href=/participant/searchparticipant.php?event_id=$event_id>View all Participants</a></li>
+          <li><a href=/race_enrolment/enrolment.php?event_id=$event_id>Enroll Participants</a></li>
+          <li><a href=/award/calculateawards.php?event_id=$event_id>Calculate Awards</a></li>
+        </ul>";
      ?>
+   </div>
   </body>
-</html>
+  <div class='close'>
+    <ul>
+      <li><a href='/'>Return Home</a></li>
+      <li><a href='indexevents.php'>Select Event</a></li>
+    </ul>
+  </div>
+</div>
+</div>
